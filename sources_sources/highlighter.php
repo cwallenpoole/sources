e7392bbd3be2d8f6f50cb1edffa73b55c1a1beb5
Christopher W. Allen-Poole <christopherw@allen-poole.com>
2010-12-21 10:52:49 -0500

    Added license to highlighter

--- sources_sources/highlighter.php
+++ sources_sources/highlighter.php
@@ -1,7 +1,32 @@
 <?php
+/*
+* Copyright (c) 2010 Christopher W. Allen-Poole
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+//FIXME: This should automatically prepend the MIT license unless another
+//	 license is noted on the file.
+
 $req = $_SERVER[ 'REQUEST_URI' ];
 $req = str_replace( '%20', ' ', $req );
-
+//FIXME: This needs to be split into multiple files.  I had thought it could
+//	 be avoided, but it just does not seem tenable anymore. 
 // issue with htaccess.  In some circumstances, this isn't properly reading $_GET.
 if( strpos( $req, "download=" ) !== FALSE )
 {
@@ -31,7 +56,9 @@ function get_extension( $file )
     return @$path_info['extension'];
 }
 $ext = get_extension($req);
-if( is_null( $ext ) || !$ext )
+// all of these are conditions where the highlighter should simply
+// output the file content and then exit.
+if( is_null( $ext ) || !$ext || in_array( $ext, array( "txt") ) )
 {
     header( 'Content-type: text/plain' );
     echo get_file_contents($req);
